c2cce83a07cb831ad1581dc5319291ebe3f82510
KoVaL5 <boriskovalkov003@gmail.com>
2025-08-22 19:43:35 +0300

    Update Квадратное уравнение.c
    Я поменял название функций и добавил проверки на ноль с дискриминантом, но у меня не получилось сравнить с помощью функции дискриминант с 0. Функцию linesolver() добавлю позже и включу её в squaresolver(), чтобы она решала случаи с линейными уравнениями.

--- Квадратное уравнение.c
+++ Квадратное уравнение.c
@@ -1,86 +1,104 @@
 #include <stdio.h>
 #include <cmath>
 #include <TXLib.h>
-int square(double a, double b, double c,
+#include <float.h>
+int squaresolver(double a, double b, double c,
            double *x1, double *x2);
-int isnull(double *x1, double *x2);
-int greh(double a, double b);
+
+
+int rootisnull(double *x1, double *x2);
+
+
+int inaccuracy(double a, double b);
+
+
 int main()
 {
-    printf("Введите коэффиеценты a, b, c\n");
+    printf("Р’РІРµРґРёС‚Рµ РєРѕСЌС„С„РёС†РёРµРЅС‚С‹ a, b, c\n");
     int prov = -2;
-    double a, b, c, x1 , x2;
+    double a, b, c  , x1 , x2;
     a = b = c = x1 = x2 = NAN;
-    scanf("%lg %lg %lg", &a, &b, &c);
-    prov = square(a, b, c, &x1, &x2);
-    isnull(&x1, &x2);
+
+    while(scanf("%lg %lg %lg", &a, &b, &c)!=3)
+    {
+        printf("РџРѕРїСЂРѕР±СѓР№С‚Рµ РµС‰С‘ СЂР°Р·, РїРѕР¶Р°Р»СѓР№СЃС‚Р°\n");
+        scanf("%lg %lg %lg", &a, &b, &c);
+    }
+    prov = squaresolver(a, b, c, &x1, &x2);
+    rootisnull(&x1, &x2);
     switch (prov)
     {
-        case -1: printf("Выражение не зависит от x"); break;
-        case  0: printf("Действительных корней нет"); break;
-        case  1: printf("Имеется только один корень x = %lg", x1); break;
-        case  2: printf("Имеется 2 корня x1 = %lg  второй корень %lg", x1, x2); break;
-        case  3: printf("У данного уравнения бесконечно много корней");break;
-        default: printf("Ты молодец");
+        case -1: printf("Р’С‹СЂР°Р¶РµРЅРёРµ РЅРµ Р·Р°РІРёСЃРёС‚ РѕС‚ x"); break;
+        case  0: printf("Р”РµР№СЃС‚РІРёС‚РµР»СЊРЅС‹С… РєРѕСЂРЅРµР№ РЅРµС‚"); break;
+        case  1: printf("РРјРµРµС‚СЃСЏ С‚РѕР»СЊРєРѕ РѕРґРёРЅ РєРѕСЂРµРЅСЊ x = %lg", x1); break;
+        case  2: printf("РРјРµРµС‚СЃСЏ 2 РєРѕСЂРЅСЏ x1 = %lg  РІС‚РѕСЂРѕР№ РєРѕСЂРµРЅСЊ %lg", x1, x2); break;
+        case  3: printf("РЈ РґР°РЅРЅРѕРіРѕ СѓСЂР°РІРЅРµРЅРёСЏ Р±РµСЃРєРѕРЅРµС‡РЅРѕ РјРЅРѕРіРѕ РєРѕСЂРЅРµР№");break;
+        default: printf("РўС‹ РјРѕР»РѕРґРµС†");
 
     }
     return 0;
 }
-int greh(double a, double b)
+
+
+int inaccuracy(double a, double b)
 {
-    const double eps = 0.0000000001;
-    (fabs(a-b)<eps) ? 1: 0;
+    const double eps = DBL_MIN;
+    return fabs(a-b) < eps;
 }
 
 
-int isnull(double *x1, double *x2)
+int rootisnull(double *x1, double *x2)   // TODO: РџРѕРјРµРЅСЏС‚СЊ РЅР°Р·РІР°РЅРёРµ
 {
-    (*x1 == 0) ? *x1 = 0: *x1 = * x1;
-    (*x2 == 0) ? *x2 = 0: *x2 = *x2;
+    (inaccuracy(*x1, 0)) ? *x1 = 0: *x1 = * x1;
+    (inaccuracy(*x2, 0)) ? *x2 = 0: *x2 = *x2;
     return 0;
 }
-int square(double a, double b, double c,
+
+
+int squaresolver(double a, double b, double c,
            double *x1, double *x2)
 {
-    float d = -1; /*Устанавливаю значение дискриминанта*/
-    if (greh(a, 0))
+    double diskr = -1; /*РЈСЃС‚Р°РЅР°РІР»РёРІР°СЋ Р·РЅР°С‡РµРЅРёРµ РґРёСЃРєСЂРёРјРёРЅР°РЅС‚Р°*/
+    if (inaccuracy(a, 0))
     {
-        if (greh(b, 0))
+        if (inaccuracy(b, 0))
         {
-            if (greh(c, 0))
+            if (inaccuracy(c, 0))
             {
-                return 3; /*Бесконено много корней*/
+                return 3; /*Р‘РµСЃРєРѕРЅРµРЅРѕ РјРЅРѕРіРѕ РєРѕСЂРЅРµР№*/
             }
             else
             {
-                return -1; /*Корней нет, потому что выражение не зависит от x*/
+                return -1; /*РљРѕСЂРЅРµР№ РЅРµС‚, РїРѕС‚РѕРјСѓ С‡С‚Рѕ РІС‹СЂР°Р¶РµРЅРёРµ РЅРµ Р·Р°РІРёСЃРёС‚ РѕС‚ x*/
             }
         }
         else
         {
-            assert(x1 != nullptr);
-            *x1 = -c/b; /*Линейное уравнение*/
+            assert(x1 != NULL);
+            *x1 = -c/b; /*Р›РёРЅРµР№РЅРѕРµ СѓСЂР°РІРЅРµРЅРёРµ*/
             return 1;
         }
     }
     else
     {
-        d = b * b - 4 * a * c;
-        if (d < 0)
+        diskr = b * b - 4 * a * c;
+        if (diskr<0)
         {
-            return 0;  /*Действительных корней нет*/
+            return 0;  /*Р”РµР№СЃС‚РІРёС‚РµР»СЊРЅС‹С… РєРѕСЂРЅРµР№ РЅРµС‚*/
         }
-        else if (d == 0)
+        else if (inaccuracy(diskr, 0))
         {
-            *x1 = -b / (2 * a); /*Действительных корней нет*/
+            *x1 = -b / (2 * a); /*Р”РµР№СЃС‚РІРёС‚РµР»СЊРЅС‹С… РєРѕСЂРЅРµР№ РЅРµС‚*/
             return 1;
         }
         else
         {
-            assert(x1 != nullptr);
-            *x1 = (-b - sqrt(d))/(2 * a);
-            assert(x2 != nullptr);
-            *x2 = (-b + sqrt(d))/(2 * a);
+            double sqrt_d;
+            sqrt_d = sqrt(diskr);
+            assert(x1 != NULL);
+            *x1 = (-b - sqrt_d)/(2 * a);
+            assert(x2 != NULL);
+            *x2 = (-b + sqrt_d)/(2 * a);
             return 2;
         }
     }
