39ee2a584e98da262f4239dd9d688250fcd8b6ff
KoVaL5 <boriskovalkov003@gmail.com>
2025-08-23 19:50:39 +0300

    Update Квадратное уравнение.c
    Добавил отдельную функцию solver, которая решает уравнение.

--- Квадратное уравнение.c
+++ Квадратное уравнение.c
@@ -2,155 +2,180 @@
 #include <cmath>
 #include <TXLib.h>
 #include <float.h>
-const int INDEPENDENCY = -1;
-const int ONE_ROOT = 1;
-const int TWO_ROOTS = 2;
-const int ZERO_ROOTS = 0;
-const int INF_ROOT = 3;
 
-int squaresolver(double a, double b, double c,
-           double *x1, double *x2);
-
-
-int linesolver(double b, double c, double *x1);
+enum nRoots
+{
+    zero_roots,
+    one_root,
+    two_roots,
+    inf_roots,
+    none
+};
 
 
-int rootisnull(double *x1, double *x2);
+enum nRoots square_solver(double a, double b, double c,
+                          double *x1, double *x2);
+
+enum nRoots line_solver(double b, double c, 
+                        double *x1, double *x2);
+
+enum nRoots solver(double a, double b, double c,
+                   double *x1, double *x2);
+
+int root_is_zero(double *x1, double *x2);
 
 
-int inaccuracy(double a, double b);
+int double_compare(double a, double b);
 
 
 void inpuut(double* a, double *b, double *c);
 
 
-void outpuut(int* roots, double* x1, double* x2);
+void outpuut(enum nRoots* roots, double* x1, double* x2);
 
 int main()
 {
-    printf("Введите коэффициенты a, b, c\n");
+    printf("Введите коэффициенты a, b, c.\n");
 
-    int roots = -2;
+    nRoots roots = none;
     double a = NAN, b = NAN, c = NAN, x1 = NAN, x2 = NAN;
-
 
     inpuut(&a, &b, &c);
 
-    roots = squaresolver(a, b, c, &x1, &x2);
-    rootisnull(&x1, &x2);
-
+    roots = solver(a, b, c, &x1, &x2);
 
     outpuut(&roots, &x1, &x2);
-    printf("Спасибо за использование\n");
-
 
     return 0;
 }
 
 
-int inaccuracy(double a, double b)
+int double_compare(double a, double b)
 {
-    const double eps = DBL_MIN;
+    const double eps = 1e-8;
+
     return fabs(a-b) < eps;
 }
 
 
-int rootisnull(double *x1, double *x2)
+int root_is_zero(double *x1, double *x2)
 {
-    (inaccuracy(*x1, 0)) ? *x1 = 0: *x1 = * x1;
-    (inaccuracy(*x2, 0)) ? *x2 = 0: *x2 = *x2;
+    if (double_compare(*x1, 0))
+    {
+        *x1 = 0;
+    }
+
+    if (double_compare(*x2, 0))
+    {
+        *x2 = 0;
+    }
+
     return 0;
 }
 
 
-int squaresolver(double a, double b, double c,
+enum nRoots square_solver(double a, double b, double c,
            double *x1, double *x2)
 {
     double diskr = -1; /*Устанавливаю значение дискриминанта*/
-    if (inaccuracy(a, 0))
+    diskr = b * b - 4 * a * c;
+    if (diskr < 0)
     {
-        linesolver(b, c, x1);
+        return zero_roots;  /*Действительных корней нет*/
+    }
+    else if (double_compare(diskr, 0))
+    {
+        *x1 = -b / (2 * a);
+        root_is_zero(x1, x2);
+        return one_root;
     }
     else
     {
-        diskr = b * b - 4 * a * c;
-        if (diskr<0)
-        {
-            return 0;  /*Действительных корней нет*/
-        }
-        else if (inaccuracy(diskr, 0))
-        {
-            *x1 = -b / (2 * a);
-            return 1;
-        }
-        else
-        {
-            double sqrt_d;
-            sqrt_d = sqrt(diskr);
-            assert(x1 != NULL);
-            *x1 = (-b - sqrt_d)/(2 * a);
-            assert(x2 != NULL);
-            *x2 = (-b + sqrt_d)/(2 * a);
-            return 2;
-        }
+        double sqrt_d = sqrt(diskr);
+
+        *x1 = (-b - sqrt_d)/(2 * a);
+
+        *x2 = (-b + sqrt_d)/(2 * a);
+
+        root_is_zero(x1, x2);
+        return two_roots;
     }
 }
 
-int linesolver(double b, double c, double *x1)
+enum nRoots line_solver(double b, double c, double *x1, double *x2)
 {
-    if (inaccuracy(b, 0))
+    if (double_compare(b, 0))
         {
-            if (inaccuracy(c, 0))
+            if (double_compare(c, 0))
             {
-                return 3; /*Бесконено много корней*/
+                return inf_roots; /*Бесконено много корней*/
             }
             else
             {
-                return -1; /*Корней нет, потому что выражение не зависит от x*/
+                return zero_roots; /*Корней нет, потому что выражение не зависит от x*/
             }
         }
         else
         {
-            assert(x1 != NULL);
-            *x1 = -c/b; /*Линейное уравнение*/
-            return 1;
+            *x1 = -c/b;
+            root_is_zero(x1, x2);
+            
+            return one_root;
         }
 }
 
 void inpuut(double* a, double *b, double *c)
 {
-    while(scanf("%lg %lg %lg", a, b, c)!=3)  //Создать отдельную функцию для ввода.
-    {   while(getchar() != '\n');
+    while(scanf("%lg %lg %lg", a, b, c)!=3)
+    {   while(getchar() != '\n');//очитска буффера
         printf("Попробуйте ещё раз, пожалуйста.\n");
     }
 }
-void outpuut
-(int* roots, double* x1, double* x2)
+void outpuut(enum nRoots* roots, double* x1, double* x2)
 {
     switch (*roots)
     {
-        case INDEPENDENCY:
-            printf("Выражение не зависит от x\n");
+
+        case  zero_roots:
+            printf("Действительных корней нет.\n");
+            printf("Спасибо за использование.\n");
             break;
 
-        case  ZERO_ROOTS:
-            printf("Действительных корней нет\n");
+        case  one_root:
+            printf("Имеется только один корень: x = %lg.\n", *x1);
+            printf("Спасибо за использование.\n");
             break;
 
-        case  ONE_ROOT:
-            printf("Имеется только один корень: x = %lg\n", *x1);
+        case  two_roots:
+            printf("Имеется 2 корня: x1 = %lg, второй корень %lg.\n", *x1, *x2);
+            printf("Спасибо за использование.\n");
             break;
 
-        case  TWO_ROOTS:
-            printf("Имеется 2 корня: x1 = %lg, второй корень %lg\n", *x1, *x2);
-            break;
-
-        case  INF_ROOT:
-            printf("У данного уравнения бесконечно много корней\n");
+        case  inf_roots:
+            printf("У данного уравнения бесконечно много корней.\n");
+            printf("Спасибо за использование.\n");
             break;
 
         default:
-            printf("Что-то пошло не так\n");
+            printf("Что-то пошло не так.\n");
+            printf("Спасибо за использование.\n");
 
     }
+}
+
+enum nRoots solver(double a, double b, double c,
+                   double *x1, double *x2)
+{
+    enum nRoots roots;
+
+    if (double_compare(a, 0))
+    {
+        roots = line_solver(b, c, x1, x2);
+    }
+    else
+    {
+        roots = square_solver(a, b, c, x1, x2);
+    }
+    return roots;
+
 }
