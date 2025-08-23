a50ada87f606704e899f2fb042ba6938faa8f986
KoVaL5 <boriskovalkov003@gmail.com>
2025-08-23 18:05:30 +0300

    Update Квадратное уравнение.c
    Добавил функции ввода вывода и решения линейных уравнений.

--- Квадратное уравнение.c
+++ Квадратное уравнение.c
@@ -2,9 +2,17 @@
 #include <cmath>
 #include <TXLib.h>
 #include <float.h>
+const int INDEPENDENCY = -1;
+const int ONE_ROOT = 1;
+const int TWO_ROOTS = 2;
+const int ZERO_ROOTS = 0;
+const int INF_ROOT = 3;
+
 int squaresolver(double a, double b, double c,
            double *x1, double *x2);
-double linesolver(double a, double b);
+
+
+int linesolver(double b, double c, double *x1);
 
 
 int rootisnull(double *x1, double *x2);
@@ -13,30 +21,29 @@ int rootisnull(double *x1, double *x2);
 int inaccuracy(double a, double b);
 
 
+void inpuut(double* a, double *b, double *c);
+
+
+void outpuut(int* roots, double* x1, double* x2);
+
 int main()
 {
-        printf("Введите коэффициенты a, b, c\n");
-        int prov = -2;
-        double a, b, c  , x1 , x2;
-        a = b = c = x1 = x2 = NAN;
+    printf("Введите коэффициенты a, b, c\n");
 
-    while(scanf("%lg %lg %lg", &a, &b, &c)!=3)
-    {   while(getchar() != '\n'){}
-        printf("Попробуйте ещё раз, пожалуйста\n");
-    }
-    prov = squaresolver(a, b, c, &x1, &x2);
+    int roots = -2;
+    double a = NAN, b = NAN, c = NAN, x1 = NAN, x2 = NAN;
+
+
+    inpuut(&a, &b, &c);
+
+    roots = squaresolver(a, b, c, &x1, &x2);
     rootisnull(&x1, &x2);
-    switch (prov)
-    {
-        case -1: printf("Выражение не зависит от x\n"); break;
-        case  0: printf("Действительных корней нет\n"); break;
-        case  1: printf("Имеется только один корень: x = %lg\n", x1); break;
-        case  2: printf("Имеется 2 корня: x1 = %lg, второй корень %lg\n", x1, x2); break;
-        case  3: printf("У данного уравнения бесконечно много корней\n");break;
-        default: printf("Что-то пошло не так\n");
 
-    }
+
+    outpuut(&roots, &x1, &x2);
     printf("Спасибо за использование\n");
+
+
     return 0;
 }
 
@@ -62,23 +69,7 @@ int squaresolver(double a, double b, double c,
     double diskr = -1; /*Устанавливаю значение дискриминанта*/
     if (inaccuracy(a, 0))
     {
-        if (inaccuracy(b, 0))
-        {
-            if (inaccuracy(c, 0))
-            {
-                return 3; /*Бесконено много корней*/
-            }
-            else
-            {
-                return -1; /*Корней нет, потому что выражение не зависит от x*/
-            }
-        }
-        else
-        {
-            assert(x1 != NULL);
-            *x1 = linesolver(b, c); /*Линейное уравнение*/
-            return 1;
-        }
+        linesolver(b, c, x1);
     }
     else
     {
@@ -105,7 +96,61 @@ int squaresolver(double a, double b, double c,
     }
 }
 
-double linesolver(double a, double b)
+int linesolver(double b, double c, double *x1)
 {
-    return (-b/a);
+    if (inaccuracy(b, 0))
+        {
+            if (inaccuracy(c, 0))
+            {
+                return 3; /*Бесконено много корней*/
+            }
+            else
+            {
+                return -1; /*Корней нет, потому что выражение не зависит от x*/
+            }
+        }
+        else
+        {
+            assert(x1 != NULL);
+            *x1 = -c/b; /*Линейное уравнение*/
+            return 1;
+        }
+}
+
+void inpuut(double* a, double *b, double *c)
+{
+    while(scanf("%lg %lg %lg", a, b, c)!=3)  //Создать отдельную функцию для ввода.
+    {   while(getchar() != '\n');
+        printf("Попробуйте ещё раз, пожалуйста.\n");
+    }
+}
+void outpuut
+(int* roots, double* x1, double* x2)
+{
+    switch (*roots)
+    {
+        case INDEPENDENCY:
+            printf("Выражение не зависит от x\n");
+            break;
+
+        case  ZERO_ROOTS:
+            printf("Действительных корней нет\n");
+            break;
+
+        case  ONE_ROOT:
+            printf("Имеется только один корень: x = %lg\n", *x1);
+            break;
+
+        case  TWO_ROOTS:
+            printf("Имеется 2 корня: x1 = %lg, второй корень %lg\n", *x1, *x2);
+            break;
+
+        case  INF_ROOT:
+            printf("У данного уравнения бесконечно много корней\n");
+            break;
+
+        default:
+            printf("Что-то пошло не так\n");
+
+    }
 }
